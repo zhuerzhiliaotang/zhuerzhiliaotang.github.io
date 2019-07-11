e44b0f9e9c4a8f5ca179a86a61e632a4da4ac215
zhuer <zhuer@qq.com>
2019-07-11 12:48:09 +0800

    第一次

---
+++ data/good.js
@@ -0,0 +1,97 @@
+//这里的数据是来自服务器的json数据
+var data = 
+{
+    "server_time": 1560757611,
+    "goods_list": [
+        {
+            "goods_id": 80783298,
+            "goods_name": "【21.76元抢200件，抢完恢复22.9元】3条装包邮 金号纯棉深色大格毛巾",
+            "short_name": "金号纯棉深色大格毛巾",
+            "thumb_url": "http://omsproductionimg.yangkeduo.com/images/2018-06-11/88998463afb91e4c62058dab0a19ab2e.jpeg",
+            "hd_thumb_url": "http://omsproductionimg.yangkeduo.com/images/2018-06-11/88998463afb91e4c62058dab0a19ab2e.jpeg",
+            "image_url": "http://omsproductionimg.yangkeduo.com/images/2017-09-15/5551e7fd201a22a25d48549155907de0.jpeg",
+            "sales": 27000,
+            "cnt": 27000,
+            "group_price": 2176,
+            "normal_price": 2990,
+            "market_price": 8900,
+            "event_type": 0,
+            "customer_num": 2,
+            "mall_id": 493592,
+            "group": {
+                "price": 2176,
+                "customer_num": 2
+            },
+            "sales_tip": "已拼2.7万件",
+            "link_url": "goods.html?goods_id=80783298",
+            "hd_url": "http://omsproductionimg.yangkeduo.com/images/2017-10-02/5657c2cb8e15c73ad7048149199aea71.jpeg",
+            "quantity": 71908,
+            "activity_type": 3,
+            "cat_id1": 9319,
+            "goods_type": 1,
+            "scores": {},
+            "group_price_origin": 2176,
+            "deposit_price": 2176,
+            "paid_price": 4352
+        },
+        {
+            "goods_id": 1381383631,
+            "goods_name": "洁丽雅儿童毛巾四条装纯棉洗脸竹纤维卡通宝宝手巾洗澡挂绳小方巾",
+            "short_name": "洁丽雅儿童毛巾四条装纯棉 柔软吸水小毛巾",
+            "thumb_url": "http://t00img.yangkeduo.com/goods/images/2019-03-16/8736854c440c40835d3d3e04b9009107.jpeg",
+            "hd_thumb_url": "http://t00img.yangkeduo.com/goods/images/2019-03-16/8736854c440c40835d3d3e04b9009107.jpeg",
+            "image_url": "http://t07img.yangkeduo.com/images/2018-05-17/7ad3634b20301f341b3ca5059b6b46ba.jpeg",
+            "sales": 86000,
+            "cnt": 86000,
+            "group_price": 1660,
+            "normal_price": 2480,
+            "market_price": 4800,
+            "event_type": 0,
+            "customer_num": 2,
+            "mall_id": 128686879,
+            "group": {
+                "price": 1660,
+                "customer_num": 2
+            },
+            "sales_tip": "已拼8.6万件",
+            "link_url": "goods.html?goods_id=1381383631",
+            "hd_url": "https://t00img.yangkeduo.com/goods/images/2018-08-27/50cfc03541f4a1047d44adbe046db8f9.jpeg",
+            "quantity": 1849,
+            "activity_type": 101,
+            "cat_id1": 9319,
+            "goods_type": 1,
+            "scores": {},
+            "group_price_origin": 1660,
+            "deposit_price": 1660,
+            "paid_price": 3320
+        },
+        {
+            "goods_id": 3384444593,
+            "goods_name": "博世电钻电动工具家用手枪钻大功率TBM3400升级款GBM340/GBM345",
+            "short_name": "博世家用手电钻",
+            "thumb_url": "http://t00img.yangkeduo.com/goods/images/2018-10-16/3850e1651549d68a74e020c11f6b522c.jpeg",
+            "hd_thumb_url": "http://t00img.yangkeduo.com/goods/images/2018-10-16/3850e1651549d68a74e020c11f6b522c.jpeg",
+            "image_url": "http://t00img.yangkeduo.com/goods/images/2018-11-29/97cf211038e12cdfadac281cc4e2c3b9.png",
+            "sales": 307,
+            "cnt": 307,
+            "group_price": 16400,
+            "normal_price": 17300,
+            "market_price": 18700,
+            "event_type": 0,
+            "customer_num": 2,
+            "mall_id": 630065121,
+            "group": {
+                "price": 16400,
+                "customer_num": 2
+            },
+            "sales_tip": "已拼307件",
+            "link_url": "goods.html?goods_id=3384444593",
+            "hd_url": "http://t00img.yangkeduo.com/goods/images/2018-10-16/364756540031d0bb58d45c63581e7f46.jpeg",
+            "quantity": 47,
+            "cat_id1": 9323,
+            "goods_type": 1,
+            "scores": {},
+            "group_price_origin": 16400
+        }
+    ]
+}
